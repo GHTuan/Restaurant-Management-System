882dbc5ee21e4e5b2f074f5263034d8824b09328
Nghiaha2003 <95176147+Nghiaha2003@users.noreply.github.com>
2024-04-05 21:23:08 +0700

    initial news v1.php

--- View/user/pages/home/news.php
+++ View/user/pages/home/news.php
@@ -1,6 +1,61 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<body>
-    This is News
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>News</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+  <div class="row justify-content-between align-items-center">
+    <div class="col">
+      <h4 class="text-start">THE LATEST DISHES</h4>
+    </div>
+    <div class="col text-end">
+      <a href="#" class="text-decoration-none">See more -></a>
+    </div>
+  </div>
+</div>
+<div class="container" style="text-align: left; line-height: 2;">
+    <div class="row align-items-start">
+        <div class="col">
+            <a href="https://your-menu-site.com" style="text-decoration: none; color: inherit;">
+                <div class="card mb-4" style="width: 24rem; background-color: #FFE000">
+                    <img src="https://www.allrecipes.com/thmb/XD6sbgr5PwSjie2JEehH-tnBGm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ALR-187822-baked-chicken-wings-4x3-5c7b4624c8554f3da5aabb7d3a91a209.jpg" class="card-img-top" alt="...">
+                    <div class="card-body" style="text-align: center;">
+                        <p class="card-text">Cánh gà chiên thơm ngon mời bạn ăn nha, tôi đây không chờ bạn nữa, giờ tôi ăn liền.</p>
+                        <button class="btn" style="background-color:#008FFF; color: white">Đặt ngay</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col">
+            <a href="https://your-menu-site.com" style="text-decoration: none; color: inherit;">
+            <div class="card mb-4" style="width: 24rem; background-color: #FFE000">
+                    <img src="https://www.allrecipes.com/thmb/XD6sbgr5PwSjie2JEehH-tnBGm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ALR-187822-baked-chicken-wings-4x3-5c7b4624c8554f3da5aabb7d3a91a209.jpg" class="card-img-top" alt="...">
+                    <div class="card-body" style="text-align: center;">
+                        <p class="card-text">Cánh gà chiên thơm ngon mời bạn ăn nha, tôi đây không chờ bạn nữa, giờ tôi ăn liền.</p>
+                        <button class="btn" style="background-color:#008FFF; color: white">Đặt ngay</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="https://your-menu-site.com" style="text-decoration: none; color: inherit;">
+            <div class="card mb-4" style="width: 24rem; background-color: #FFE000">
+                    <img src="https://www.allrecipes.com/thmb/XD6sbgr5PwSjie2JEehH-tnBGm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ALR-187822-baked-chicken-wings-4x3-5c7b4624c8554f3da5aabb7d3a91a209.jpg" class="card-img-top" alt="...">
+                    <div class="card-body" style="text-align: center;">
+                        <p class="card-text">Cánh gà chiên thơm ngon mời bạn ăn nha, tôi đây không chờ bạn nữa, giờ tôi ăn liền.</p>
+                        <button class="btn" style="background-color:#008FFF; color: white">Đặt ngay</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
