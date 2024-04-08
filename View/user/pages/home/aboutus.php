cb428f44dc3fffa3c175f1c7666d16221771add0
Nghiaha2003 <nghiaha2003@hotmail.com>
2024-04-08 19:04:14 +0700

    style(aboutus) update about us page ver 1

--- View/user/pages/home/aboutus.php
+++ View/user/pages/home/aboutus.php
@@ -1,6 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-    This is About Us
+<div class="container">
+        <h1 class="my-5 text-center">About Us</h1>
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-center">
+            <img src="View/Images/about_us_1.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 500px; height: 500px">
+            </div>
+            <div class="col-md-6">
+                <p class="lead" style="font-size: 24px">Welcome to our restaurant, a culinary haven nestled in the heart of the vibrant USA dining scene.
+                    We are more than just a place to eat; we are a destination where flavors, passion, and community converge
+                    to create unforgettable dining experiences.</p>
+                <p style="font-size: 18px">Our journey began with a simple yet profound vision: to craft a dining establishment that celebrates the art
+                    of food, the joy of sharing meals, and the warmth of hospitality. With this vision as our guiding light,
+                    we embarked on a culinary adventure that has since blossomed into the beloved restaurant you see today.</p>
+                <p style="font-size: 18px">At our restaurant, we believe in the power of food to bring people together, to create memories, and to
+                    nourish both body and soul. Every dish we serve is a labor of love, crafted with care and attention to
+                    detail. From sourcing the freshest ingredients to perfecting each recipe, we pour our hearts into every
+                    plate that leaves our kitchen.</p>
+            </div>
+        </div>
+        <div class="row mt-5 pt-5">
+            <div class="col-md-5">
+                <h2 class="my-4 text-center">Our Philosophy</h2>
+                <p style="font-size: 18px">Our philosophy is rooted in the belief that food is more than just sustenance; it's a
+                    celebration of life's moments, both big and small. We strive to create dishes that not only taste
+                    delicious but also tell a story, evoke emotions, and spark conversations.</p>
+                    <p style="font-size: 18px">At our restaurant, we believe that food is not just about satisfying hunger, but also about creating memorable experiences that bring people together. Our philosophy is deeply rooted in the belief that every meal should be a celebration of life's moments, both big and small. We strive to create dishes that not only taste delicious but also tell a story, evoke emotions, and spark conversations.</p>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-6 d-flex align-items-center">
+            <img src="View/Images/about_us_2.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 500px; height: 500px">
+            </div>
+        </div>
+        <div class="row mt-5 pt-5 mb-5 pb-5">
+            <div class="col-md-6 d-flex align-items-center">
+            <img src="View/Images/about_us_1.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 500px; height: 500px">
+            </div>
+            <div class="col-md-6">
+                <h2 class="my-4 text-center">Our Promise</h2>
+                <p style="font-size: 18px">At our restaurant, we are committed to providing exceptional service and hospitality that goes beyond your expectations. We believe that every guest deserves to be treated with warmth, respect, and genuine care, and we strive to create a welcoming and inclusive environment for everyone who walks through our doors.</p>
+                <p style="font-size: 18px">Our team of dedicated service professionals are here to ensure that your dining experience is seamless and enjoyable, from the moment you arrive until the moment you leave. Whether you need help with menu selections, wine pairings, or special requests, we are here to make sure that your every need is met with professionalism and attention to detail.</p>
+                <p style="font-size: 18px">At our restaurant, we are more than just a restaurant - we are a place where people come together to share their love of food, connect with each other, and create lasting memories. We invite you to join us and experience the difference for yourself.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-5 pt-5 mb-5 pb-5">
+    <h2 class="my-4 text-center">Our Sastified Customer</h2>
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-5">
+                    <blockquote class="blockquote text-center">
+                        <img src="View/Images/about_us_1.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 300px; height: 300px">
+                        <p class="mb-0">Đồ ăn và dịch vụ tuyệt vời. Tôi cảm thấy như một khách VIP ngay từ lúc bước vào cửa. Không thể đợi để quay lại, nhưng mà tôi hết tiền rồi!</p><br>
+                        <footer class="blockquote-footer"><cite title="Nguồn">John Doe</cite></footer>
+                    </blockquote>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-5">
+                    <blockquote class="blockquote text-center">
+                        <img src="View/Images/about_us_1.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 300px; height: 300px">
+                        <p class="mb-0">Tôi đã đến nhiều nhà hàng, nhưng nhà hàng này nổi bật với sự chú ý đến chi tiết và cam kết với chất lượng. Tôi rất khuyên bạn nên thử nếu ai đó đang tìm kiếm một trải nghiệm ẩm thực đặc biệt thực sự.</p><br>
+                        <footer class="blockquote-footer"><cite title="Nguồn">Jane Smith</cite></footer>
+                    </blockquote>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5">
+                    <blockquote class="blockquote text-center">
+                        <img src="View/Images/about_us_2.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 300px; height: 300px">
+                        <p class="mb-0">Không khí ấm áp và hấp dẫn, đồ ăn ngon vãi cả chưởng, nhân viên thân thiện và chu đáo. Tôi đã lập kế hoạch cho chuyến thăm tiếp theo của mình!</p><br>
+                        <footer class="blockquote-footer"><cite title="Nguồn">Bob Johnson</cite></footer>
+                    </blockquote>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-5">
+                    <blockquote class="blockquote text-center">
+                        <img src="View/Images/about_us_2.jpg" alt="About Us" class="img-fluid rounded-3" style="width: 300px; height: 300px">
+                        <p class="mb-0">Nhà hàng này là một viên ngọc ẩn. Từ thức ăn đến dịch vụ và trang trí, mọi thứ đều hoàn hảo. Tôi không thể nói đủ về điều tốt đẹp của nó!</p><br>
+                        <footer class="blockquote-footer"><cite title="Nguồn">Sarah Lee</cite></footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
