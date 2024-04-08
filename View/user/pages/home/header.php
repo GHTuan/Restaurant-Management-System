b7f891bf8c10b6c2604e834b2ced2e9d35125e5c
Hong Phuc Le <lhplhp10102004@gmail.com>
2024-04-08 18:11:22 +0700

    abc

--- View/user/pages/home/header.php
+++ View/user/pages/home/header.php
@@ -1,6 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-    This is Header
+    <div>
+        <img src="View/Images/background_1.jpg" alt="Background" style="
+            width: 100%;
+            height: 100vh;
+            object-fit: cover;
+            position: relative;
+        ">
+        <div id="my-introduction" style="
+            position: absolute;
+            bottom: 20px;
+            left: 30px;
+            background-color: rgba(255, 255, 255, 0.5);
+            padding: 10px;
+            border-radius: 10px;
+        ">
+            Hello World
+
+        </div>
+    </div>
 </body>
 </html>
