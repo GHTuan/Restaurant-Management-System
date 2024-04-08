c83ee795416c6ca150a826ded4bc75e139a9e12c
Nghiaha2003 <nghiaha2003@hotmail.com>
2024-04-08 15:17:09 +0700

    style(contact & news): change the display of opening Hours & some other minors

--- View/user/pages/home/news.php
+++ View/user/pages/home/news.php
@@ -4,16 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>News</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
   </head>
   <body>
   <div class="container">
   <div class="row justify-content-between align-items-center">
     <div class="col">
-      <h4 class="text-start">THE LATEST DISHES</h4>
+      <h4 class="text-start">LATEST DISHES</h4>
     </div>
     <div class="col text-end">
-      <a href="#" class="text-decoration-none">See more -></a>
+      <a href="#" class="text-decoration-none">Discover more -></a>
     </div>
   </div>
 </div>
@@ -24,7 +24,7 @@
                 <div class="card mb-4" style="width: 24rem; background-color: #FFE000">
                     <img src="https://www.allrecipes.com/thmb/XD6sbgr5PwSjie2JEehH-tnBGm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ALR-187822-baked-chicken-wings-4x3-5c7b4624c8554f3da5aabb7d3a91a209.jpg" class="card-img-top" alt="...">
                     <div class="card-body" style="text-align: center;">
-                        <p class="card-text">Cánh gà chiên thơm ngon mời bạn ăn nha, tôi đây không chờ bạn nữa, giờ tôi ăn liền.</p>
+                        <p class="card-text" style="font-weight: 500";>Enjoy our delicious and fragrant baked chicken wings, served hot and fresh.</p>
                         <button class="btn" style="background-color:#008FFF; color: white">Đặt ngay</button>
                     </div>
                 </div>
@@ -36,7 +36,7 @@
             <div class="card mb-4" style="width: 24rem; background-color: #FFE000">
                     <img src="https://www.allrecipes.com/thmb/XD6sbgr5PwSjie2JEehH-tnBGm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ALR-187822-baked-chicken-wings-4x3-5c7b4624c8554f3da5aabb7d3a91a209.jpg" class="card-img-top" alt="...">
                     <div class="card-body" style="text-align: center;">
-                        <p class="card-text">Cánh gà chiên thơm ngon mời bạn ăn nha, tôi đây không chờ bạn nữa, giờ tôi ăn liền.</p>
+                        <p class="card-text" style="font-weight: 500";>Enjoy our delicious and fragrant baked chicken wings, served hot and fresh.</p>
                         <button class="btn" style="background-color:#008FFF; color: white">Đặt ngay</button>
                     </div>
                 </div>
@@ -47,7 +47,7 @@
             <div class="card mb-4" style="width: 24rem; background-color: #FFE000">
                     <img src="https://www.allrecipes.com/thmb/XD6sbgr5PwSjie2JEehH-tnBGm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ALR-187822-baked-chicken-wings-4x3-5c7b4624c8554f3da5aabb7d3a91a209.jpg" class="card-img-top" alt="...">
                     <div class="card-body" style="text-align: center;">
-                        <p class="card-text">Cánh gà chiên thơm ngon mời bạn ăn nha, tôi đây không chờ bạn nữa, giờ tôi ăn liền.</p>
+                        <p class="card-text" style="font-weight: 500";>Enjoy our delicious and fragrant baked chicken wings, served hot and fresh.</p>
                         <button class="btn" style="background-color:#008FFF; color: white">Đặt ngay</button>
                     </div>
                 </div>
@@ -56,6 +56,6 @@
     </div>
 </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
   </body>
 </html>
