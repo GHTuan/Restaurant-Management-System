d6b88f7900cd122a8d0b60d55a661ee18a1dd020
Hong Phuc Le <lhplhp10102004@gmail.com>
2024-04-08 16:54:11 +0700

    Update

--- View/user/layouts/navbar.php
+++ View/user/layouts/navbar.php
@@ -1,47 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+<script src="https://kit.fontawesome.com/20b9485268.js" crossorigin="anonymous"></script>
 <body>
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">User Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php?controller=home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php?controller=menu">Menu</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <nav class="navbar navbar-expand-lg sticky-top d-flex flex-row-reverse" style="
+      padding: 0;
+      top: 10px;
+      right: 0;
+      position: fixed;
+      font-size: 15px;
+    ">
+      <div class="container-fluid p-1" style="
+          margin: 0;
+          background-color: #daf0f7;
+          width: 80vw;
+      ">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="padding: 0 10px;">
+          <span class="navbar-toggler-icon" style="width: 18px;"></span>
+        </button>
+        <a class="navbar-brand" href="#" style="font-size: 15px; font-weight: bold;";>User Navbar</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php?controller=home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php?controller=menu">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php?controller=menu">Cart</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php?controller=menu">Setting</a>
+          </li>
+          <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="View/admin/">To Admin</a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <ul class="navbar-nav mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="View/admin/">To Admin</a>
-        </li>
-      <ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </ul>
+      </div>
+      <a class="nav-link active" aria-current="page" href="index.php" style="margin-right: 10px;">Login <i class="fa-solid fa-user-tie"></i></a>
     </div>
-  </div>
-</nav>
+  </nav>
 </body>
-</html>
 
+</html>
