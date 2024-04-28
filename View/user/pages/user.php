cb313948225d4f6430694400acc77166cde18302
Nghiaha2003 <nghiaha2003@hotmail.com>
2024-04-28 18:41:04 +0700

    user page: add some banners, faq, add info and minor fixes

--- View/user/pages/user.php
+++ View/user/pages/user.php
@@ -12,18 +12,16 @@
 <body>
 <div class="container mt-5 pt-5">
     <div class="row">
-        <div class="col-md-2 fs-5">
-            <!-- Sidebar -->
-            <div class="list-group ">
-                <a href="#" class="list-group-item list-group-item-action d-flex justify-content-center align-items-center font-weight-bold py-5"><i class="fas fa-user me-3"></i> <!-- Icon -->Edit profile</a>
-                <a href="#" class="list-group-item list-group-item-action d-flex justify-content-center align-items-center font-weight-bold py-5"><i class="fas fa-bell me-3"></i> <!-- Icon -->Notification</a>
-                <a href="#" class="list-group-item list-group-item-action d-flex justify-content-center align-items-center font-weight-bold py-5"><i class="fas fa-lock me-3"></i> <!-- Icon -->Security</a>
-                <a href="#" class="list-group-item list-group-item-action d-flex justify-content-center align-items-center font-weight-bold py-5"><i class="fas fa-paint-brush me-3"></i> <!-- Icon -->Appearance</a>
-                <a href="#" class="list-group-item list-group-item-action d-flex justify-content-center align-items-center font-weight-bold py-5"><i class="fas fa-question-circle me-3"></i> <!-- Icon -->Help</a>
+        <div class="col-md-2">
+            <div class="product img d-flex justify-content-center align-items-center mb-4">
+                <img id="productImage1" src="View/Images/sale_1.png" alt="Pizza Sale" class="img-fluid">
             </div>
-        </div>
+            <div class="product img d-flex justify-content-center align-items-center mb-4 ">
+                <img id="productImage2" src="View/Images/sale_2.png" alt="Coffee Sale" class="img-fluid">
+            </div>
+        </div> 
         <div class="col-md-1"></div>
-        <div class="col-md-9">
+        <div class="col-md-6">
         <h1 class="text-center mb-4">My Information</h1>
         <div class="product img d-flex justify-content-center align-items-center mb-4">
             <img id="productImage" src="View/Images/siuuu.png" alt="Avatar" class="rounded-circle" style="width: 250px; height: 250px;">
@@ -71,6 +69,86 @@
             </div>
         </form>
     </div>
+    <div class="col-md-1"></div>
+    <div class="col-md-2 text-center">
+
+        <div class="arrange-unit__09f24__rqHTg yelp-emotion-1iy1dwt">
+            <div class=" yelp-emotion-1hj76qr">
+                <h3 class="yelp-emotion-i7hfd5 fs-5 text-center">Frequently Asked Question</h3>
+                <ul class=" list__09f24__ynIEd text-center">
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                            <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                <p class=" yelp-emotion-v293gj ">Change my password</p>
+                            </div>
+                            </div></a>
+                    </li>
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                                <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                    <p class=" yelp-emotion-v293gj">Delete your account</p>
+                            </div>
+                            </div></a>
+                    </li>
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                            <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                <p class=" yelp-emotion-v293gj">Signin option</p>
+                            </div>
+                        </div></a>
+                    </li>
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                            <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                <p class=" yelp-emotion-v293gj">Contact administrator</p>
+                            </div>
+                        </div></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="arrange-unit__09f24__rqHTg yelp-emotion-1iy1dwt mt-5 mb-5">
+            <div class=" yelp-emotion-1hj76qr">
+                <h3 class="yelp-emotion-i7hfd5 fs-5 text-center">Additional Information</h3>
+                <ul class=" list__09f24__ynIEd text-center">
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                            <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                <p class=" yelp-emotion-v293gj ">Terms & Conditions</p>
+                            </div>
+                            </div></a>
+                    </li>
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                                <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                    <p class=" yelp-emotion-v293gj">Privacy Policy</p>
+                            </div>
+                            </div></a>
+                    </li>
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                            <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                <p class=" yelp-emotion-v293gj">Our Co-founder</p>
+                            </div>
+                        </div></a>
+                    </li>
+                    <li class="list-group-item yelp-emotion-1iy1dwt"><a href="https://www.youtube.com/shorts/kQyCp9zOBk4" class="yelp-emotion-op9593">
+                        <div class="arrange__09f24__LDfbs gutter-2__09f24__CCmUo vertical-align-middle__09f24__zU9sE yelp-emotion-1iy1dwt">
+                            <div class="arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG yelp-emotion-1dgdgwt">
+                                <p class=" yelp-emotion-v293gj">Donate Us</p>
+                            </div>
+                        </div></a>
+                    </li>
+                </ul>
+            </div>
+        </div>    
+
+    <div class="product img d-flex justify-content-center align-items-center mb-4">
+        <img id="productImage3" src="View/Images/sale_3.png" alt="Weekend Sale" class="img-fluid" style="width: 100%; height: 100%;">
+    </div>
+
+    </div> 
     
 </body>
 </html>
