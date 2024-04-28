b5f21fc6828799de8380559b04d8018ea3a5b751
Hong Phuc Le <lhplhp10102004@gmail.com>
2024-04-28 22:58:05 +0700

    Making redirection for admin session

--- index.php
+++ index.php
@@ -13,22 +13,31 @@
 </body>
 </html>
 <?php
-	if (!isset($_GET['controller'])) {
-		require 'View/user/pages/home.php'; /*require giao diện trang chủ*/
-		return;
-	}
+	
 	session_start();
 	if (isset($_SESSION['Role'])){
 		if ($_SESSION['Role'] == 'admin'){
+			if (!isset($_GET['controller'])) {
+				header('Location: ?controller=home');
+				return;
+			}
 			require 'Route/admin/web.php'; 
 			return;
 		}
 		if ($_SESSION['Role'] == 'member'){
+			if (!isset($_GET['controller'])) {
+				require 'View/user/pages/home.php'; /*require giao diện trang chủ*/
+				return;
+			}
 			require 'Route/user/web.php'; 
 			return;
 		}
 	}
 	#This is guest
+	if (!isset($_GET['controller'])) {
+		require 'View/user/pages/home.php'; /*require giao diện trang chủ*/
+		return;
+	}
 	require 'Route/guest.php';
 	
 	
